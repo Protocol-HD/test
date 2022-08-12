deb44b28bfa58c2dcb77247fc08bcce91ea3065d
Arthur <arthur@comets.kr>
2022-08-12 11:12:50 +0900

    first commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { contexts } from './Contexts';
+
+function Home() {
+    const { num, setNum } = useContext(contexts)
+
+    return (
+        <div>
+            {num}
+            <br/>
+            <button onClick={() => setNum(num + 1)}>click</button>
+        </div>
+    );
+}
+
+export default Home;
